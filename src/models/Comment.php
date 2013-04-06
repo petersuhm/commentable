b9d42da8d392e60119cb8246025aa3af78b28e36
Peter Suhm <peter@suhm.dk>
2013-04-06 22:59:50 +0200

    add commentable method

--- src/models/Comment.php
+++ src/models/Comment.php
@@ -5,4 +5,9 @@ use Illuminate\Database\Eloquent\Model as Eloquent;
 class Comment extends Eloquent {
 
     protected $fillable = array('body');
+
+    public function commentable()
+    {
+        return $this->morphTo();
+    }
 }
